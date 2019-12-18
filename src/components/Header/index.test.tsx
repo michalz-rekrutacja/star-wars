341e64d9c9023a3aeae587321dd423168a5d97b0
michalz-rekrutacja <58881223+michalz-rekrutacja@users.noreply.github.com>
2019-12-18 17:10:12 +0100

    style: rename variables

--- src/components/Header/index.test.tsx
+++ src/components/Header/index.test.tsx
@@ -7,21 +7,21 @@ describe('rendering', () => {
     const { getByText } = render(
       <Header heading="Title" highlight="Highlight" />
     );
-    const linkElement = getByText(/Title/i);
-    expect(linkElement).toBeInTheDocument();
+    const value = getByText(/Title/i);
+    expect(value).toBeInTheDocument();
   });
 
   it('renders highlight', () => {
     const { getByText } = render(
       <Header heading="Title" highlight="Highlight" />
     );
-    const linkElement = getByText(/Highlight/i);
-    expect(linkElement).toBeInTheDocument();
+    const value = getByText(/Highlight/i);
+    expect(value).toBeInTheDocument();
   });
 
   it('renders default values on props missing', () => {
     const { getByText } = render(<Header highlight="Highlight" />);
-    const linkElement = getByText(/Star Wars/i);
-    expect(linkElement).toBeInTheDocument();
+    const value = getByText(/Star Wars/i);
+    expect(value).toBeInTheDocument();
   });
 });
